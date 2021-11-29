fd59a39eb758655c49d1cb8757fb66c58b1f3a87
Andrew Timberlake <andrew@andrewtimberlake.com>
2021-11-29 12:54:29 +0200

    Update formatting from prettier

--- addon/mixins/styled-component.js
+++ addon/mixins/styled-component.js
@@ -1,7 +1,7 @@
-import { A } from '@ember/array';
-import { dasherize, camelize, htmlSafe } from '@ember/string';
-import { defineProperty, computed } from '@ember/object';
-import Mixin from '@ember/object/mixin';
+import { A } from "@ember/array";
+import { dasherize, camelize, htmlSafe } from "@ember/string";
+import { defineProperty, computed } from "@ember/object";
+import Mixin from "@ember/object/mixin";
 
 // Thanks to ember-computed-style
 // Lifted from React
@@ -35,66 +35,68 @@ const isUnitlessNumber = {
   stopOpacity: true,
   strokeDashoffset: true,
   strokeOpacity: true,
-  strokeWidth: true
+  strokeWidth: true,
 };
 
 export default Mixin.create({
-  concatenatedProperties: ['styleBindings'],
+  concatenatedProperties: ["styleBindings"],
 
   init() {
     this._super(...arguments);
 
-    if(this.styleBindings || this.styles) {
-      let styleKeys = ['styles'];
-      if(this.styleBindings) {
+    if (this.styleBindings || this.styles) {
+      let styleKeys = ["styles"];
+      if (this.styleBindings) {
         this.styleBindings = A(this.styleBindings.slice());
-        this.styleBindings.forEach(function(binding) {
-          styleKeys.push(binding.split(':')[0]);
+        this.styleBindings.forEach(function (binding) {
+          styleKeys.push(binding.split(":")[0]);
         });
       }
-      if(this.attributeBindings) {
+      if (this.attributeBindings) {
         const attrb = this.attributeBindings.slice(0);
-        attrb.push('style');
+        attrb.push("style");
         this.attributeBindings = attrb;
       } else {
-        this.attributeBindings = ['style'];
+        this.attributeBindings = ["style"];
       }
 
-      defineProperty(this, 'style', computed(...styleKeys, this._buildStyles));
+      defineProperty(this, "style", computed(...styleKeys, this._buildStyles));
     }
   },
 
   _buildStyle(property, value) {
-    if (typeof value === 'undefined' || value === null ) {
+    if (typeof value === "undefined" || value === null) {
       return null;
     } else {
-      return this._fixStyles(property, value).join(':');
+      return this._fixStyles(property, value).join(":");
     }
   },
 
   _buildStyles() {
     let styles = [];
-    if(this.styleBindings) {
+    if (this.styleBindings) {
       this.styleBindings.forEach((binding) => {
-        let [key, property] = binding.split(':');
-        if(!property) { property = key; }
+        let [key, property] = binding.split(":");
+        if (!property) {
+          property = key;
+        }
         styles.push(this._buildStyle(property, this.get(key)));
       });
     }
-    if(this.styles) {
+    if (this.styles) {
       Object.keys(this.styles).forEach((key) => {
-        styles.push(this._buildStyle(key, this.get('styles.'+key)));
+        styles.push(this._buildStyle(key, this.get("styles." + key)));
       });
     }
     // console.log('styles', styles);
-    return htmlSafe(styles.filter(el => el).join(';'));
+    return htmlSafe(styles.filter((el) => el).join(";"));
   },
 
   _fixStyles(property, value) {
     let isNumber = !isNaN(value) && isFinite(value);
-    if(isNumber && !isUnitlessNumber[camelize(property)]) {
-      value = value + 'px';
+    if (isNumber && !isUnitlessNumber[camelize(property)]) {
+      value = value + "px";
     }
     return [dasherize(property), value];
-  }
+  },
 });
